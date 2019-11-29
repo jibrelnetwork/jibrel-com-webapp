dfaf6862f5f1ead6c3e6d7778e63774a45787bee
Ivan Violentov <violentoff@gmail.com>
2019-11-29 16:13:18 +0300

    Update base index file

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,5 +1,4 @@
-import Input from './Input'
-
-export {
-  Input,
-}
+export { default as Input } from './Input'
+export { default as BigButton } from './BigButton'
+export { default as LinkButton } from './LinkButton'
+export { default as SmallButton } from './SmallButton'
