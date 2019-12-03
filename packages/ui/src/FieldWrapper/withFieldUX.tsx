f24499e08f0d452ec483a59a47cee606dbb06a23
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2019-12-03 17:40:30 +0300

    Fix types, remove unused

--- packages/ui/src/FieldWrapper/withFieldUX.tsx
+++ packages/ui/src/FieldWrapper/withFieldUX.tsx
@@ -8,16 +8,14 @@ import { WithMessageProps } from './withMessage'
 export interface WithFieldUXWrapperProps extends GenericFieldProps {
   hint?: string;
   success?: string;
-  className?: string;
-  children: React.ReactNode;
 }
 
 export const withFieldUX = <P extends React.PropsWithoutRef<JSX.IntrinsicElements['input']>>(Component: React.ComponentType<P & WithMessageProps>): React.FunctionComponent<P & WithFieldUXWrapperProps & GenericFieldProps> => {
   const WithFieldUXWrapper: React.FunctionComponent<P & WithFieldUXWrapperProps & GenericFieldProps> = ({
     meta,
+    input,
     hint,
     success,
-    input,
     ...props
   }) => {
     const message = getMessage({
