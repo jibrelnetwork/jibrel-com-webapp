b664cc9adc79a23e0405a3b8826699436e2a5b18
Ivan Violentov <violentoff@gmail.com>
2019-12-03 18:05:05 +0300

    Update Checkbox

--- packages/ui/src/Checkbox/index.tsx
+++ packages/ui/src/Checkbox/index.tsx
@@ -1,52 +1,36 @@
 import React from 'react'
 import cc from 'classcat'
-import { FieldRenderProps } from 'react-final-form'
 
 import Icon from '../Icon'
 import style from './style.scss'
-import FieldMessage, { FieldMessageType } from '../FieldMessage'
-import getErrorMessage, { FormValidateType } from '../utils/forms/getErrorMessage'
 
-export interface CheckboxProps extends FieldRenderProps<boolean> {
+import { 
+  withField,
+  withFieldUX,
+} from '../FieldWrapper'
+
+export interface CheckboxProps {
   children: React.ReactNode,
-  message: string,
   className: string,
-  messageType: FieldMessageType,
-  validateType: FormValidateType,
+  value: boolean,
+  hasError: boolean,
   isDisabled: boolean,
   isRequired: boolean,
 }
 
 const Checkbox: React.FunctionComponent<CheckboxProps> = ({
-  meta,
-  input,
   children,
-  message,
+  value,
   className,
-  messageType,
-  validateType,
+  hasError,
   isDisabled,
   isRequired,
   ...props
 }) => {
-  const {
-    value,
-    ...other
-  } = input
-
-  const errorMsg: string | null = getErrorMessage(
-    meta,
-    validateType,
-  )
-
-  const msg: string = errorMsg || message
-  const msgType: FieldMessageType = !!errorMsg ? FieldMessageType.error : messageType
-
   return (
-    <label className={cc([style.input, style[msgType], className])}>
+    <label className={cc([style.input, hasError && style.error, className])}>
       <input
         {...props}
-        {...other}
         name={name}
         className={style.field}
         type='checkbox'
@@ -58,17 +42,14 @@ const Checkbox: React.FunctionComponent<CheckboxProps> = ({
         className={cc([style.checkbox, !value && style.off, isDisabled && style.disabled])}
       />
       <p className={style.label}>{children}</p>
-      <FieldMessage type={msgType}>{msg}</FieldMessage>
     </label> 
   )
 }
 
 Checkbox.defaultProps = {
-  message: '',
-  messageType: FieldMessageType.info,
-  validateType: FormValidateType.dirtySinceLastSubmit,
+  hasError: false,
   isDisabled: false,
   isRequired: false,
 }
 
-export default Checkbox
+export default withField(withFieldUX(React.memo(Checkbox)))
