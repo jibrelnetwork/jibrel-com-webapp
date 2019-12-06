efb68bdb986852dcca3930b0ffc88d8da6671cd9
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2019-12-06 13:26:49 +0300

    Move Windows check out of component scope

--- apps/id/src/components/CountrySelect/index.tsx
+++ apps/id/src/components/CountrySelect/index.tsx
@@ -6,35 +6,33 @@ import COUNTRIES_ORDER from './countries/order/en.json'
 import COUNTRIES_TITLES from './countries/en.common.json'
 
 type CountrySelectProps = Omit<React.ComponentProps<typeof Select.Select>, 'children'>
+const isWindowsSystem = navigator.platform.startsWith('Win')
 
-const CountrySelect: React.FunctionComponent<CountrySelectProps> = (props) => {
-  const isWindowsSystem = navigator.platform.startsWith('Win')
+const CountrySelect: React.FunctionComponent<CountrySelectProps> = (props) => (
+  <Select.Select
+    label={props.label}
+    name={props.name}
+    {...props}
+  >
+    {COUNTRIES_ORDER.map((id: string) => {
+      const country = COUNTRIES_INDEX[id]
+      const label = COUNTRIES_TITLES[`ref.country.${id}`]
+      const flag = isWindowsSystem
+        ? ''
+        : `${country.flag} `
 
-  return (
-    <Select.Select
-      label={props.label}
-      name={props.name}
-      {...props}
-    >
-      {COUNTRIES_ORDER.map((id: string) => {
-        const country = COUNTRIES_INDEX[id]
-        const label = COUNTRIES_TITLES[`ref.country.${id}`]
-        const flag = isWindowsSystem
-          ? ''
-          : `${country.flag} `
+      return (
+        <Select.Option
+          key={country.id}
+          label={label}
+          value={country.id}
+        >
+          {flag + label}
+        </Select.Option>
+      )
+    })}
+  </Select.Select>
+)
 
-        return (
-          <Select.Option
-            key={country.id}
-            label={label}
-            value={country.id}
-          >
-            {flag + label}
-          </Select.Option>
-        )
-      })}
-    </Select.Select>
-  )
-}
 
 export default CountrySelect
