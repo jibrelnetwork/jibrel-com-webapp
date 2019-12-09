cc3afc982b6ff0e9a29c8194c0ae22bfda23221c
Ivan Violentov <violentoff@gmail.com>
2019-12-09 20:14:29 +0300

    Add UserActionInfo component

---
+++ apps/id/src/components/UserActionInfo/style.scss.d.ts
@@ -0,0 +1,19 @@
+declare namespace StyleScssModule {
+  export interface IStyleScss {
+    centered: string;
+    chromeframe: string;
+    column: string;
+    content: string;
+    grid: string;
+    icon: string;
+    main: string;
+    title: string;
+  }
+}
+
+declare const StyleScssModule: StyleScssModule.IStyleScss & {
+  /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
+  locals: StyleScssModule.IStyleScss;
+};
+
+export = StyleScssModule;
