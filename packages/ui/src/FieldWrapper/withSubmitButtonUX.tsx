2ee03f66f3a4ffce3bd7536816a998ca5d49cebc
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2019-12-10 22:36:03 +0300

    Fix disabled button on form correction after failed submit

--- packages/ui/src/FieldWrapper/withSubmitButtonUX.tsx
+++ packages/ui/src/FieldWrapper/withSubmitButtonUX.tsx
@@ -1,27 +1,56 @@
 import React from 'react'
 import { useFormState } from 'react-final-form'
 
+const getIsDiabled = ({
+  hasSubmitErrors,
+  hasValidationErrors,
+  dirtySinceLastSubmit,
+}: {
+  hasSubmitErrors: boolean;
+  hasValidationErrors: boolean;
+  dirtySinceLastSubmit: boolean;
+}): boolean => {
+  if (hasValidationErrors) {
+    return true
+  }
+
+  if (hasSubmitErrors && !dirtySinceLastSubmit) {
+    return true
+  }
+
+  return false
+}
+
 export const withSubmitButtonUX = <P extends React.PropsWithoutRef<JSX.IntrinsicElements['button']>>(ButtonComponent: React.ComponentType<P>): React.FunctionComponent<P> => {
   const WithSubmitButtonUXWrapper: React.FunctionComponent<P> = ({
     ...props
   }) => {
     const {
       submitting,
-      invalid,
+      hasSubmitErrors,
+      hasValidationErrors,
+      dirtySinceLastSubmit,
       validating,
     } = useFormState({
       subscription: {
         submitting: true,
-        invalid: true,
+        hasSubmitErrors: true,
+        hasValidationErrors: true,
+        dirtySinceLastSubmit: true,
         validating: true,
       }
+    })
+    const isDisabled = getIsDiabled({
+      hasSubmitErrors,
+      hasValidationErrors,
+      dirtySinceLastSubmit,
     })
 
     return <ButtonComponent
       {...props as P}
       type='submit'
       isLoading={validating || submitting}
-      isDisabled={invalid}
+      isDisabled={isDisabled}
     />
   }
 
