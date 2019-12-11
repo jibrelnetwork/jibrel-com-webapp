0aeab1e308db2f36d35fda9b635a027b5532de60
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2019-12-11 20:31:57 +0300

    Revert to proxy url

--- apps/id/src/store/axios.ts
+++ apps/id/src/store/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const instance = axios.create({
   // FIXME: take it from environment
-  baseURL: 'https://api.jibrelcom.develop.jdev.network/',
+  baseURL: 'http://api.jibrelcom.local/',
   xsrfHeaderName: 'X-CSRFToken',
   xsrfCookieName: 'csrftoken',
   withCredentials: true,
