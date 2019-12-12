37e4bb7b59719c06f73332fb43a12d7962c37772
Ivan Violentov <violentoff@gmail.com>
2019-12-12 20:50:52 +0300

    Update AuthLayout

--- apps/id/src/layouts/AuthLayout/style.scss.d.ts
+++ apps/id/src/layouts/AuthLayout/style.scss.d.ts
@@ -1,14 +1,13 @@
 declare namespace StyleScssModule {
   export interface IStyleScss {
-    layout: string;
-    header: string;
     content: string;
+    header: string;
   }
 }
 
 declare const StyleScssModule: StyleScssModule.IStyleScss & {
   /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
   locals: StyleScssModule.IStyleScss;
-}
+};
 
 export = StyleScssModule;
