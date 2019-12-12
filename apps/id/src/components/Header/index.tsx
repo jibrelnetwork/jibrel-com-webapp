17d4b14369530034f0b28779b55e9035631c4474
Ivan Violentov <violentoff@gmail.com>
2019-12-12 20:50:25 +0300

    Update Header component

--- apps/id/src/components/Header/index.tsx
+++ apps/id/src/components/Header/index.tsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import cc from 'classcat'
+import { connect } from 'react-redux'
 
 import { Logo } from '@jibrelcom/ui'
 import { LogoColor } from '@jibrelcom/ui/src/Logo/types'
+
+import { UserStatus } from 'store/models/user'
+
+import {
+  Dispatch,
+  RootState,
+} from 'store'
 
 import style from './style.scss'
 
 export interface HeaderProps {
   logout?: () => void;
   className?: string;
+  isAuthenticated?: boolean;
 }
 
 const Header: React.FunctionComponent<HeaderProps> = ({
-  logout,
+  logout = undefined,
   className,
+  isAuthenticated = false,
   ...props
 }) => (
   <div
@@ -36,7 +46,7 @@ const Header: React.FunctionComponent<HeaderProps> = ({
       >
         SUPPORT
       </a>
-      {logout && (
+      {isAuthenticated && logout && (
         <button
           type='button'
           onClick={logout}
@@ -49,4 +59,11 @@ const Header: React.FunctionComponent<HeaderProps> = ({
   </div>
 )
 
-export default React.memo(Header)
+export default connect(
+  (state: RootState) => ({
+    isAuthenticated: (state.user.status !== UserStatus.ANONYMOUS),
+  }),
+  (dispatch: Dispatch) => ({
+    logout: dispatch.user.logout,
+  })
+)(Header)
