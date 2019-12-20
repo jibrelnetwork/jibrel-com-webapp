8b3c19cbea164aa3dd8ead367e9447158e400df3
Konstantin Lobov <konstantin.lobov@jibrel.network>
2019-12-20 13:25:15 +0300

    Company KYC Company Information step scaffold

--- apps/id/src/pages/KYC/Company/steps/CompanyInformationForm/index.tsx
+++ apps/id/src/pages/KYC/Company/steps/CompanyInformationForm/index.tsx
@@ -1,24 +1,38 @@
 import React from 'react'
 import {useI18n} from 'app/i18n'
+import reduce from 'lodash-es/reduce'
+import isEmpty from 'lodash-es/isEmpty'
 import {Form} from 'react-final-form'
-import {FieldArray} from 'react-final-form-arrays'
-import arrayMutators from 'final-form-arrays'
-import isEqual from 'lodash-es/isEqual'
+
+import style from './style.scss'
 
 import {
     BigButtonSubmit,
-    LinkButton,
+    Input,
+    FileInput,
 } from '@jibrelcom/ui'
 
 import KYCLayout from 'layouts/KYCLayout'
 import {FormProps} from '../FormProps'
-import {BeneficiaryFields} from './BeneficiaryFields'
+import {connect} from 'react-redux'
 
-const initialBeneficiaries = [{}]
 
-export const CompanyInformationForm: React.FunctionComponent<FormProps> = ({backLabel, backHandler, nextLabel, nextHandler}) => {
+const checkEmptyFields = (values) =>
+    reduce(values, (errors, value, field) => isEmpty(value)
+        ? {...errors, [field]: 'Field value is required'}
+        : errors
+        , {})
+
+const initialValues = {
+    companyName: '',
+    tradingName: '',
+    dateOfIncorporation: '',
+    placeOfIncorporation: '',
+}
+
+const CompanyInformationFormComponent: React.FunctionComponent<FormProps> = ({backLabel, backHandler, nextLabel, nextHandler, fieldValues, uploadDocument}) => {
+    const {commercialRegister, shareholderRegister, articlesOfIncorporation} = fieldValues
     const i18n = useI18n()
-    const initialValues = {}
     return (
         <KYCLayout
             title={i18n._('KYC.Company.section.company.title')}
@@ -27,34 +41,64 @@ export const CompanyInformationForm: React.FunctionComponent<FormProps> = ({back
         >
             <Form
                 initialValues={initialValues}
-                mutators={{...arrayMutators}}
                 onSubmit={nextHandler}
-                render={({
-                             handleSubmit,
-                             form: {
-                                 mutators: {push}
-                             },
-                         }) => (
-                    <form onSubmit={handleSubmit}>
-                        <FieldArray name="beneficiary" isEqual={isEqual} initialValue={initialBeneficiaries}>
-                            {({fields}) =>
-                                fields.map((name, index) => (
-                                    <BeneficiaryFields isPrimary={index===0} key={name} index={index}>
-                                        {index !== 0 &&
-                                        <LinkButton type="button" onClick={() => fields.remove(index)}>
-                                          Remove Beneficiary
-                                        </LinkButton>
-                                        }
-                                    </BeneficiaryFields>
-                                ))
-                            }
-                        </FieldArray>
+                validate={checkEmptyFields}
+                render={({hasValidationErrors, handleSubmit}) => (
+                    <form onSubmit={handleSubmit} className={style.step}>
+                        <Input
+                            name='companyName'
+                            label={'Company Name'}
+                        />
+                        <Input
+                            name='tradingName'
+                            label={'Trading Name'}
+                        />
+                        <Input
+                            name='dateOfIncorporation'
+                            label={'Date of Incorporation'}
+                            placeholder={'DD/MM/YYYY'}
+                        />
+                        <Input
+                            name='placeOfIncorporation'
+                            label={'Place of Incorporation'}
+                        />
 
-                        <LinkButton type="button" onClick={() => push('beneficiary', undefined)}>
-                            + ADD MORE BENEFICIARIES
-                        </LinkButton>
+                        <h3 className={style.groupTitle}>
+                            {'Company Documentation'}
+                        </h3>
 
-                        <BigButtonSubmit>
+                        <FileInput
+                            name='commercialRegister'
+                            label={'Commercial Register'}
+                            placeholder={'PNG, PDF, JPG'}
+                            onChange={(file: File) => uploadDocument({file, fieldName: 'commercialRegister'})}
+                            fileName={commercialRegister ? commercialRegister.fileName : ''}
+                            fileSize={commercialRegister ? commercialRegister.fileSize : 0}
+                            isLoading={commercialRegister ? commercialRegister.isLoading : false}
+                            error={commercialRegister ? commercialRegister.error : ''}
+                        />
+                        <FileInput
+                            name='shareholderRegister'
+                            label={'Shareholder Register'}
+                            placeholder={'PNG, PDF, JPG'}
+                            onChange={(file: File) => uploadDocument({file, fieldName: 'commercialRegister'})}
+                            fileName={shareholderRegister ? shareholderRegister.fileName : ''}
+                            fileSize={shareholderRegister ? shareholderRegister.fileSize : 0}
+                            isLoading={shareholderRegister ? shareholderRegister.isLoading : false}
+                            error={shareholderRegister ? shareholderRegister.error : ''}
+                        />
+                        <FileInput
+                            name='articlesOfIncorporation'
+                            label={'Article of Incorporation'}
+                            placeholder={'PNG, PDF, JPG'}
+                            onChange={(file: File) => uploadDocument({file, fieldName: 'articlesOfIncorporation'})}
+                            fileName={articlesOfIncorporation ? articlesOfIncorporation.fileName : ''}
+                            fileSize={articlesOfIncorporation ? articlesOfIncorporation.fileSize : 0}
+                            isLoading={articlesOfIncorporation ? articlesOfIncorporation.isLoading : false}
+                            error={articlesOfIncorporation ? articlesOfIncorporation.error : ''}
+                        />
+
+                        <BigButtonSubmit disabled={hasValidationErrors}>
                             {nextLabel}
                         </BigButtonSubmit>
                     </form>
@@ -63,3 +107,11 @@ export const CompanyInformationForm: React.FunctionComponent<FormProps> = ({back
         </KYCLayout>
     )
 }
+
+const mapDispatch = (dispatch) => ({
+    uploadDocument: () => {
+        console.log('File Upload Stub')
+    },
+})
+
+export const CompanyInformationForm = connect(null, mapDispatch)(CompanyInformationFormComponent)
