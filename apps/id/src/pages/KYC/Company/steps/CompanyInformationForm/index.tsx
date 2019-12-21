94ef859282a37ba8a6481bd9033a1e59dcacab82
Konstantin Lobov <konstantin.lobov@jibrel.network>
2019-12-21 12:49:49 +0300

    Company KYC Primary Contact form

--- apps/id/src/pages/KYC/Company/steps/CompanyInformationForm/index.tsx
+++ apps/id/src/pages/KYC/Company/steps/CompanyInformationForm/index.tsx
@@ -15,9 +15,9 @@ import {
 import KYCLayout from 'layouts/KYCLayout'
 
 import {FormProps} from '../FormProps'
-import {checkEmptyFields} from '../checkEmptyFieldsValidator'
 import style from './style.scss'
 import {handleAsyncValidationErrors} from '../handleAsyncValidationErrors'
+import isRequired from 'utils/validators/isRequired'
 
 const emptyFileField = {
     fileName: '',
@@ -52,25 +52,28 @@ const CompanyInformationFormComponent: React.FunctionComponent<FormProps> = (pro
             <Form
                 initialValues={initialValues}
                 onSubmit={submit(nextHandler)}
-                validate={checkEmptyFields}
                 render={({hasValidationErrors, handleSubmit, submitting}) => (
                     <form onSubmit={handleSubmit} className={style.step}>
                         <Input
                             name='companyName'
                             label={'Company Name'}
+                            validate={isRequired({i18n})}
                         />
                         <Input
                             name='tradingName'
                             label={'Trading Name'}
+                            validate={isRequired({i18n})}
                         />
                         <Input
                             name='dateOfIncorporation'
                             label={'Date of Incorporation'}
                             placeholder={'DD/MM/YYYY'}
+                            validate={isRequired({i18n})}
                         />
                         <Input
                             name='placeOfIncorporation'
                             label={'Place of Incorporation'}
+                            validate={isRequired({i18n})}
                         />
 
                         <h3 className={style.groupTitle}>
--- apps/id/src/pages/KYC/Company/steps/PrimaryContactForm/index.tsx
+++ apps/id/src/pages/KYC/Company/steps/PrimaryContactForm/index.tsx
@@ -3,15 +3,42 @@ import {useI18n} from 'app/i18n'
 import {Form} from 'react-final-form'
 
 import {
-    BigButtonSubmit,
+    BigButton,
+    Input,
+    FileInput,
 } from '@jibrelcom/ui'
 
 import KYCLayout from 'layouts/KYCLayout'
 import {FormProps} from '../FormProps'
+import pick from 'lodash-es/pick'
+import style from 'pages/KYC/Company/steps/CompanyInformationForm/style.scss'
+import {handleAsyncValidationErrors} from 'pages/KYC/Company/steps/handleAsyncValidationErrors'
+import {connect} from 'react-redux'
+import {CountrySelect} from 'components'
+import isRequired from 'utils/validators/isRequired'
 
-export const PrimaryContactForm: React.FunctionComponent<FormProps> = ({backLabel, backHandler, nextLabel, nextHandler}) => {
+const emptyFileField = {
+    fileName: '',
+    fileSize: 0,
+    isLoading: false,
+    error: '',
+}
+
+export const PrimaryContactFormComponent: React.FunctionComponent<FormProps> = (props) => {
+    const {
+        backLabel, backHandler, nextLabel, nextHandler,
+        formValues, uploadDocument, submit,
+        passportDocument, proofOfAddressDocument,
+    } = props
+
+    const passport = passportDocument || emptyFileField
+    const proofOfAddress = proofOfAddressDocument || emptyFileField
+
+    const initialValues = pick(formValues, [
+        'companyName', 'tradingName', 'dateOfIncorporation', 'placeOfIncorporation',
+    ])
+
     const i18n = useI18n()
-    const initialValues = {}
     return (
         <KYCLayout
             title={i18n._('KYC.Company.section.primaryContact.title')}
@@ -20,17 +47,137 @@ export const PrimaryContactForm: React.FunctionComponent<FormProps> = ({backLabe
         >
             <Form
                 initialValues={initialValues}
-                onSubmit={nextHandler}
-                render={() => (
-                    <div>
+                onSubmit={submit(nextHandler)}
+                render={({hasValidationErrors, handleSubmit, errors, submitting}) => (
+                    <form onSubmit={handleSubmit} className={style.step}>
 
+                        <h3 className={style.groupTitle}>
+                            {'Personal Information'}
+                        </h3>
+                        <Input
+                            name='firstName'
+                            label={'First Name'}
+                            validate={isRequired({i18n})}
+                        />
+                        <Input
+                            name='lastName'
+                            label={'Last Name'}
+                            validate={isRequired({i18n})}
+                        />
+                        <Input
+                            name='middleName'
+                            label={'Middle Name (Optional)'}
+                        />
+                        <Input
+                            name='birthDate'
+                            label={'Date of Birth'}
+                            placeholder={'DD/MM/YYYY'}
+                            validate={isRequired({i18n})}
+                        />
+                        <CountrySelect
+                            name='nationality'
+                            label={'Nationality'}
+                            placeholder={'select nationality'}
+                            validate={isRequired({i18n})}
+                        />
 
-                        <BigButtonSubmit onClick={nextHandler}>
+                        <h3 className={style.groupTitle}>
+                            {'Current Residential Address'}
+                        </h3>
+
+                        <Input
+                            name='phoneNumber'
+                            label={'Primary Telephone'}
+                            validate={isRequired({i18n})}
+                            // FIXME: So is it optional or not?
+                            // label={'Primary Telephone (Optional)'}
+                        />
+                        <Input
+                            name='email'
+                            label={'Email'}
+                            validate={isRequired({i18n})}
+                        />
+                        <Input
+                            name='streetAddress'
+                            label={'Street Address'}
+                            validate={isRequired({i18n})}
+                        />
+                        <Input
+                            name='apartment'
+                            label={'Apartment, Unit or Suite (Optional)'}
+                        />
+                        <Input
+                            name='city'
+                            label={'City'}
+                            validate={isRequired({i18n})}
+                        />
+                        <Input
+                            name='postCode'
+                            label={'Post Code (Optional)'}
+                        />
+                        <CountrySelect
+                            name='country'
+                            label={'Country of Residency'}
+                            placeholder={'select country'}
+                            validate={isRequired({i18n})}
+                        />
+
+                        <FileInput
+                            name='proofOfAddressDocument'
+                            label={'Proof of Address (Utility Bill, Bank Statement)'}
+                            placeholder={'PNG, PDF, JPG'}
+                            onFileChange={(file: File) => uploadDocument({file, fieldName: 'proofOfAddressDocument'})}
+                            {...proofOfAddress}
+                        />
+
+                        <h3 className={style.groupTitle}>
+                            {'Personal ID'}
+                        </h3>
+                        <Input
+                            name='passportNumber'
+                            label={'Passport Number'}
+                            validate={isRequired({i18n})}
+                        />
+                        <Input
+                            name='passportExpirationDate'
+                            label={'Passport Expiration Date'}
+                            placeholder={'DD/MM/YYYY'}
+                            validate={isRequired({i18n})}
+                        />
+                        <FileInput
+                            name='passportDocument'
+                            label={'Passport Front Page'}
+                            placeholder={'PNG, PDF, JPG'}
+                            onFileChange={(file: File) => uploadDocument({file, fieldName: 'passportDocument'})}
+                            {...passport}
+                        />
+
+                        <BigButton isLoading={submitting} isDisabled={!passport.id || !proofOfAddress.id || hasValidationErrors}
+                                   className={style.submit}>
                             {nextLabel}
-                        </BigButtonSubmit>
-                    </div>
+                        </BigButton>
+                    </form>
                 )}
             />
         </KYCLayout>
     )
 }
+
+
+const mapState = ({kycOrganization, kyc}) => ({
+    formValues: kycOrganization.values,
+
+    // File upload fields with extra structure
+    passportDocument: kyc.documents.passportDocument,
+    proofOfAddressDocument: kyc.documents.proofOfAddressDocument,
+})
+
+const mapDispatch = ({kyc, kycOrganizationValidate}) => ({
+    uploadDocument: kyc.uploadDocument,
+    submit: (callback) => (values) =>
+        kycOrganizationValidate.validate({step: 2, ...values})
+            .then(callback)
+            .catch(handleAsyncValidationErrors),
+})
+
+export const PrimaryContactForm = connect(mapState, mapDispatch)(PrimaryContactFormComponent)
