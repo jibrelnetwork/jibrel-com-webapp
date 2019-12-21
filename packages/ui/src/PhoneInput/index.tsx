48795d4cf8c2860134f8b7e88629a2e2de01b42d
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2019-12-21 18:24:03 +0300

    Fix phone input to display error and correctly parse input value

--- packages/ui/src/PhoneInput/index.tsx
+++ packages/ui/src/PhoneInput/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react'
 import cc from 'classcat'
 
-import Input from '../Input'
+import inputStyle from '../Input/style.scss'
 import style from './style.scss'
 import { GenericFieldProps } from '../FieldWrapper/types'
+import {
+  withField,
+  withFieldUX,
+  withMessage,
+} from '../FieldWrapper'
 
 export interface PhoneInputProps {
   ccc: string;
   label?: string;
   className?: string;
+  hasError?: boolean;
+  isDisabled?: boolean;
 }
 
-export const clearPhoneNumber = (ccc: string) => (phoneNumber: string): string => {
+export const clearPhoneNumber = ({ ccc }: PhoneInputProps) => (phoneNumber: string): string => {
   if (!phoneNumber) {
     return phoneNumber
   }
@@ -29,6 +36,9 @@ const PhoneInput: React.FunctionComponent<GenericFieldProps & PhoneInputProps> =
   ccc,
   className,
   label = 'Phone Number',
+  name,
+  hasError = false,
+  isDisabled = false,
   ...props
 }) => {
   return (
@@ -41,15 +51,31 @@ const PhoneInput: React.FunctionComponent<GenericFieldProps & PhoneInputProps> =
       <span className={style.ccc}>
         {ccc}
       </span>
-      <Input
-        {...props}
-        label={label}
-        inputClassName={style.input}
-        labelClassName={style.label}
-        parse={clearPhoneNumber(ccc)}
-      />
+      <label className={cc([inputStyle.input, hasError && inputStyle.error, className])}>
+        <input
+          {...props}
+          name={name}
+          type='text'
+          className={cc([
+            inputStyle.field,
+            style.input,
+          ])}
+          disabled={isDisabled}
+        />
+        <div className={inputStyle.frame} />
+        <p
+          className={cc([
+            inputStyle.label,
+            style.label,
+          ])}
+        >
+          {label}
+        </p>
+      </label>
     </div>
   )
 }
 
-export default React.memo(PhoneInput)
+export default withField(withFieldUX(React.memo(withMessage(PhoneInput))), {
+  parse: clearPhoneNumber,
+})
