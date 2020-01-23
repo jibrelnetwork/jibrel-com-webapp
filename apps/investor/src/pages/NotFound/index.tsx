716f29ecc450b0af0b9e7d4e7cd62d595ad92dcd
Ivan Violentov <violentoff@gmail.com>
2020-01-23 12:12:33 +0300

    Move NotFound to packages/ui

--- apps/investor/src/pages/NotFound/index.tsx
+++ apps/investor/src/pages/NotFound/index.tsx
@@ -1,35 +1,10 @@
 import React from 'react'
-import cc from 'classcat'
-
-import {
-  Grid,
-  BigButton,
-} from '@jibrelcom/ui'
+import { NotFound } from '@jibrelcom/ui'
 
 import settings from 'app/settings'
 
-import style from './style.scss'
-
-const NotFound: React.FunctionComponent = () => (
-  <Grid.Container>
-    <article className={cc(['error', style.main])}>
-      <img
-        src={`${settings.HOST_CMS}/img/pic_hero_404_error.png`}
-        alt='404 Not Found'
-        className='error__hero'
-      />
-      <h1 className='error__title'>It Looks Like You’re Lost</h1>
-      <p className={cc(['error__description', style.text])}>
-        This page is missing or you assembled the link incorrectly.
-      </p>
-      <a
-        href={settings.HOST_CMS}
-        className={cc(['error__button', style.button])}
-      >
-        <BigButton>Back to main</BigButton>
-      </a>
-    </article>
-  </Grid.Container>
+const NotFoundEnhanced: React.FunctionComponent = () => (
+  <NotFound href={settings.HOST_CMS} />
 )
 
-export default NotFound
+export default NotFoundEnhanced
