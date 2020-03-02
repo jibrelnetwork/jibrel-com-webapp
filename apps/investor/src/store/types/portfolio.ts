1d423b56805eb9679d3ce03852d70be07249d80c
Ivan Violentov <violentoff@gmail.com>
2020-03-02 18:20:25 +0300

    Update portfolio model

--- apps/investor/src/store/types/portfolio.ts
+++ apps/investor/src/store/types/portfolio.ts
@@ -9,7 +9,13 @@ export interface Asset {
 }
 
 export enum InvestmentStatus {
+  draft = 'draft',
   pending = 'pending',
+  hold = 'hold',
+  completed = 'completed',
+  canceled = 'canceled',
+  expired = 'expired',
+  error = 'error',
 }
 
 export interface Investment {
@@ -24,22 +30,51 @@ export interface Investment {
   isAgreedSubscription: boolean;
 }
 
+export interface OfferingSubscription {
+  offering: Offering;
+  email: string;
+  amount: string;
+  createdAt: string;
+}
+
+export interface CurrentOffering {
+  flags: {
+    completed: boolean;
+    active: boolean;
+    waitlist: boolean;
+    past: boolean;
+    current: boolean;
+    future: boolean;
+  };
+  valuation: string;
+  goal_formatted: string;
+  round_formatted: string;
+  valuation_title: string;
+  valuation_formatted: string;
+}
+
 export interface CompanyData {
   logo: string;
   slug: string;
   title: string;
+  preview: string;
+  tagline: string;
+  location: string;
+  permalink: string;
+  currentOffering?: CurrentOffering;
   color: {
     primary: string;
     background: string;
   };
 }
 
-export type Companies = { [key: string]: CompanyData }
-
 export interface PortfolioState {
-  companies: Companies;
+  companies: CompanyData[] | undefined;
   investments: Investment[] | undefined;
+  waitlist: OfferingSubscription[] | undefined;
   investedAmount: string | undefined;
+  isWaitlistLoading: boolean;
+  isCompaniesLoading: boolean;
   isInvestmentsLoading: boolean;
   isInvestedAmountLoading: boolean;
 }
