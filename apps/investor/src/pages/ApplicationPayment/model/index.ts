2d0955e7f9320760fd081c82010a072cf96eaf06
Stanislav Cherenkov <stanislav.cherenkov@jibrel.network>
2020-03-12 20:37:23 +0300

    Remove unused model

--- apps/investor/src/pages/ApplicationPayment/model/index.ts
+++ apps/investor/src/pages/ApplicationPayment/model/index.ts
@@ -5,14 +5,10 @@ export { CardPaymentOperation } from './types'
 export {
   // events
   fetchInvestmentFx,
-  submitCardFormFx,
-  awaitCardPaymentOperationStatusChangeFx,
 
   // stores
   $IsLoading,
   $Investment,
-  $CardPaymentOperation,
-  $InitialMethod,
 } from './model'
 
 export { PageGate } from './presenter'
--- apps/investor/src/pages/ApplicationPayment/model/model.ts
+++ apps/investor/src/pages/ApplicationPayment/model/model.ts
@@ -8,15 +8,9 @@ import { InvestApplication } from 'store/types/invest'
 
 import axios from 'store/axios'
 
-import { createDefaultRetryConfig } from 'utils/axios'
-
 import { APIResponse } from 'store/types/api'
 import {
-  APIResponseDepositCardInvestmentApplication,
   APIResponseRetrieveInvestmentApplication,
-  CardPaymentOperation,
-  CardPaymentOperationStatus,
-  SubmitCardTokenData,
 } from './types'
 
 import { PageGate } from './presenter'
@@ -34,40 +28,8 @@ export const fetchInvestmentFx = domain.createEffect<
       .then(unpackAxiosResponse)
 })
 
-export const submitCardFormFx = domain.createEffect<
-  SubmitCardTokenData,
-  CardPaymentOperation
-  >({
-  handler: ({
-              investmentId,
-              cardToken,
-            }) => axios
-    .post<APIResponseDepositCardInvestmentApplication>(`/v1/investment/applications/${investmentId}/deposit/card`, {
-      cardToken,
-    })
-    .then(unpackAxiosResponse)
-})
-
-export const awaitCardPaymentOperationStatusChangeFx = domain.createEffect<string, CardPaymentOperation>({
-  handler: (uuid) =>
-    axios
-      .get<APIResponseDepositCardInvestmentApplication>(`/v1/payments/operations/${uuid}`, {
-        'axios-retry': createDefaultRetryConfig((response): boolean => {
-          const status = 'isAxiosError' in response
-            ? response.response?.data.data.status
-            : response.data.data.status
-
-          return status === CardPaymentOperationStatus.waitingPayment || status === CardPaymentOperationStatus.processing
-        })
-      })
-      .then(unpackAxiosResponse)
-})
-
 export const $Investment = domain.createStore<InvestApplication | null>(null)
   .on(fetchInvestmentFx.doneData, (state, payload) => payload)
-
-export const $CardPaymentOperation = domain.createStore<CardPaymentOperation | null>(null)
-  .on(awaitCardPaymentOperationStatusChangeFx.doneData, (state, payload) => payload)
 
 forward({
   from: PageGate.open.map(({ investmentId }) => investmentId),
