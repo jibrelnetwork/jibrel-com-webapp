b6bc18a026babcd3f9cd624f72fe5e1d58e2bb50
Vasil II <52696438+Vasil-II@users.noreply.github.com>
2020-07-08 15:25:47 +0300

    Correct mistake in urls

--- apps/id/src/settings.js
+++ apps/id/src/settings.js
@@ -1,5 +1,5 @@
 window.SETTINGS = {
-  FRONTEND_ROOT_DOMAIN_NAME: '{{ default .Env.FRONTEND_ROOT_DOMAIN_NAME "api.tokenize.jibrel.network" }}',
+  FRONTEND_ROOT_DOMAIN_NAME: '{{ default .Env.FRONTEND_ROOT_DOMAIN_NAME "tokenize.jibrel.network" }}',
   API_BASE_URL: '{{ default .Env.API_BASE_URL "//api.tokenize.jibrel.network" }}',
   ID_ORIGIN: '{{ default .Env.ID_ORIGIN "" }}',
   INVESTOR_ORIGIN: '{{ default .Env.INVESTOR_ORIGIN "" }}',
