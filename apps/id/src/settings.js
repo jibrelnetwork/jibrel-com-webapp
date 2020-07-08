a7e98e146eb61995afb1b9effc36876773681143
Vasil II <52696438+Vasil-II@users.noreply.github.com>
2020-07-08 15:11:00 +0300

    Change root domain name

--- apps/id/src/settings.js
+++ apps/id/src/settings.js
@@ -1,6 +1,6 @@
 window.SETTINGS = {
-  FRONTEND_ROOT_DOMAIN_NAME: '{{ default .Env.FRONTEND_ROOT_DOMAIN_NAME "jibrel.com" }}',
-  API_BASE_URL: '{{ default .Env.API_BASE_URL "//api.jibrel.com" }}',
+  FRONTEND_ROOT_DOMAIN_NAME: '{{ default .Env.FRONTEND_ROOT_DOMAIN_NAME "api.tokenize.jibrel.network" }}',
+  API_BASE_URL: '{{ default .Env.API_BASE_URL "//api.tokenize.jibrel.network" }}',
   ID_ORIGIN: '{{ default .Env.ID_ORIGIN "" }}',
   INVESTOR_ORIGIN: '{{ default .Env.INVESTOR_ORIGIN "" }}',
   CMS_ORIGIN: '{{ default .Env.CMS_ORIGIN "" }}',
